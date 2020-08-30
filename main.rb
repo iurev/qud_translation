b34b441fcd5ad1ab161c182b9af3531c5ceb36f0
Vitalii Iurev <vitalii@iurev.space>
2020-08-30 13:37:22 +0300

    almost done?

--- main.rb
+++ main.rb
@@ -2,64 +2,118 @@ require 'json'
 require 'pry'
 require 'russian'
 require 'fuzzystringmatch'
+require 'nokogiri'
 
 require_relative 'tr.rb'
 
-most_popular = File.read('./10k.txt')
+MOST_POPULAR = File.read('./10k.txt')
 
-puts most_popular.length
+puts MOST_POPULAR.length
 
 path = '/home/yu/games/Caves of Qud'
 assets = 'game/CoQ_Data/StreamingAssets/Base/'
 
-# file_paths = Dir.glob("#{path}/#{assets}/**/*.{txt,xml,json}")
-file_paths = Dir.glob("#{path}/#{assets}/**/*.{json}")
+file_paths = Dir.glob("#{path}/#{assets}/**/*.{txt,xml,json}")
+# file_paths = Dir.glob("#{path}/#{assets}/**/*.{xml}")
 
 puts file_paths
 
-# files = [
-#     'QudCorpus.txt',
-#     'QudCorpus1.txt',
-#     'QudCorpus2.txt',
-#     'Text.txt',
-#     'Conversations.xml',
-#     'Books.xml'
-# ]
-
-words = []
-
 dict = Dict.new
 
-def traverse(obj, dict, word)
-  regexp = /(#{word})/i
+def traverse_str(str, dict)
+  return str unless str.is_a? String
 
+  str = str.dup
+  list = str.split(/\W+/).map(&:downcase)
+
+  list = list.reject {|w| /\d/.match?(w) }
+  list = list.reject {|w| MOST_POPULAR.include?(w) }
+  list = list.reject {|w| w.include?('_') }
+  list.uniq!
+
+  list.each do |word|
+    regexp = /(#{word})/i
+
+    next str unless str
+
+    next str unless str.is_a? String
+    next str unless str.match? regexp
+
+    result = dict.get(word)
+    next str unless result
+    next str if result.empty?
+    result = Russian.translit(result)
+
+    result = result.gsub('"', '')
+    result = result.gsub('\'', '4')
+
+    puts "#{word}: #{result}"
+
+    regexp = /(^|\ |"|\|)(#{word})(\ |\.|\,|}|")/i
+    str.gsub!(regexp, "\\1\\2 (#{result})\\3")
+  end
+
+  str
+end
+
+def traverse_json(obj, dict)
   if obj.is_a? Array
-    return obj.map do |key|
-      traverse(obj[key], dict, word)
+    return obj.map do |o|
+      traverse_json(o, dict)
     end
   end
   if obj.is_a? Hash
     result = {}
     obj.keys.map do |key|
-      result[key] = traverse(obj[key], dict, word)
+      result[key] = traverse_json(obj[key], dict)
     end
     return result
   end
 
-  str = obj
+  traverse_str(obj, dict)
+end
 
-  return str unless str
-  return str unless line.match? regexp
+def traverse_xml(xml, dict)
+  if xml.class == Nokogiri::XML::Comment
+    return xml
+  end
 
-  result = dict.get(word)
-  return str unless result
-  return str if result.empty?
-  result = Russian.translit(result)
+  if xml.class == Nokogiri::XML::Document
+    traverse_xml(xml.children, dict)
 
-  result = result.gsub('"', '')
-  result = result.gsub('\'', '4')
+    return xml
+  end
 
-  line.gsub(regexp, "\\1 *#{result}")
+  if xml.class == Nokogiri::XML::Element
+    traverse_xml(xml.children, dict)
+    traverse_xml(xml.attributes, dict)
+
+    return xml
+  end
+
+  if Nokogiri::XML::NodeSet == xml.class
+    return xml.each {|n| traverse_xml(n, dict)}
+  end
+
+  if Hash == xml.class
+    xml.keys.each do |key|
+      if %w[Short BehaviorDescription DisplayName].include? key
+        begin
+          xml[key].value = traverse_str(xml[key].value, dict)
+        rescue
+          binding.pry
+        end
+      end
+    end
+    return xml
+  end
+
+  if xml.class == Nokogiri::XML::Text
+    xml.content = traverse_str(xml.content, dict)
+    return xml
+  end
+
+  traverse_str(xml, dict)
 end
 
 file_paths.each do |file_path|
@@ -72,7 +126,6 @@ file_paths.each do |file_path|
   next if file_path.include? 'Colors'
   next if file_path.include? 'Commands'
   next if file_path.include? 'Mods'
-  next if file_path.include? 'ObjectBlueprints'
   next if file_path.include? 'Options'
   next if file_path.include? 'Population'
   next if file_path.include? 'Worlds'
@@ -82,8 +135,6 @@ file_paths.each do |file_path|
   next if file_path.include? 'SultanDungeonSpice'
   next if file_path.include? 'HistorySpice'
 
-  # next unless file_path.include? 'Books'
-
   puts "Start: #{file_path}"
 
   file_content = begin
@@ -92,65 +143,35 @@ file_paths.each do |file_path|
     next
   end
 
-  list = file_content.split(/\W+/).map(&:downcase)
+  xml = if file_path.include?('xml')
+    Nokogiri::XML::Document.parse(file_content)
+  end
 
-  # list = ['alchemist']
-
-  list = list.reject {|w| /\d/.match?(w) }
-  list = list.reject {|w| most_popular.include?(w) }
-  list = list.reject {|w| w.include?('_') }
-  list.uniq!
-
-  lines = file_content.split("\n")
   json = begin
            JSON.parse(file_content)
          rescue => e
            puts e
-  end
+         end
 
-  list.each.with_index do |word, index|
-    puts "#{index} / #{list.count}"
-    next if word.nil? || word.empty?
+  str = file_content
 
-    if file_path.include? 'xml'
-      regexp = /(^|\ )(#{word})(\ |\.|\,)/i
-    else
-      regexp = /(^|\ |:"|\ ")(#{word})(\ |\.|\,|"|$)/i
-    end
-
-    if json.present?
-      json = traverse(json, dict, word)
-    else
-      lines.map! do |line|
-        next line unless line
-        next line unless line.match? regexp
-        if file_path.include? 'xml'
-          next line if line.include?("\"")
-        end
-
-        result = dict.get(word)
-        next line unless result
-        next line if result.empty?
-        result = Russian.translit(result)
-
-        result = result.gsub('"', '')
-        result = result.gsub('\'', '4')
-
-        puts "#{word} {{C|#{result}}}"
-
-        line.gsub(regexp, "\\1\\2 *#{result}\\3")
-      end
-    end
+  if json
+    json = traverse_json(json, dict)
+  elsif xml
+    traverse_xml(xml, dict)
+  else
+    str = traverse_str(file_content, dict)
   end
 
   if json
     file_content = json.to_json
+  elsif xml
+    file_content = xml.to_xml
   else
-    file_content = lines.join("\n")
+    file_content = str
   end
 
   File.write(file_path, file_content)
 
   puts "End: #{file_path}"
 end
-
